0f13250c96c276cb6007c78033ded465a45b9d8b
GrafZeppelinZwei <34131904+GrafZeppelinZwei@users.noreply.github.com>
2017-12-23 12:39:42 +0800

    UserService (exception handle undone)

---
+++ src/main/java/xmu/crms/mapper/UserMapper.java
@@ -0,0 +1,35 @@
+package xmu.crms.mapper;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import xmu.crms.entity.Attendance;
+import xmu.crms.entity.User;
+
+/**
+ * UserMapper
+ * 仅获取登录所需要的用户信息
+ *
+ * @author YeHongjie
+ * @date 2017-12-19
+ */
+@Component
+public interface UserMapper {
+
+	User getUserByUserId(BigInteger userId);
+
+	List<User> getUserByUserName(String userName);
+	
+	int insertAttendanceById(Attendance attendance);
+	
+	void updateUserByUserId(BigInteger userId, User user);
+	
+	List<User> listUserByClassId(BigInteger classId, String numBeginWith, String nameBeginWith);
+	
+	List<User> getAttendanceByIdAndStatus(BigInteger classId, BigInteger seminarId, Integer status);
+
+	List<Attendance> getAttendanceById(BigInteger classId, BigInteger seminarId);
+    
+}
---
+++ src/main/java/xmu/crms/view/UserIdNameVO.java
@@ -0,0 +1,30 @@
+package xmu.crms.view;
+
+import java.math.BigInteger;
+
+import xmu.crms.entity.School;
+import xmu.crms.entity.User;
+
+public class UserIdNameVO {
+	private BigInteger id;
+	private String name;
+	
+	public UserIdNameVO(User user)
+	{
+		id=user.getId();
+		name=user.getName();
+	}
+	
+	public BigInteger getId() {
+		return id;
+	}
+	public void setId(BigInteger id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
