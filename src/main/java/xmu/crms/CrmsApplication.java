0f9c89a84eecb5839fd11804a1aea7fbc71c7a0b
chenyizhu <24320152202726@stu.xmu.edu.cn>
2017-12-24 13:20:58 +0800

    yee

--- src/main/java/xmu/crms/CrmsApplication.java
+++ src/main/java/xmu/crms/CrmsApplication.java
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @MapperScan("xmu.crms.mapper")
 public class CrmsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CrmsApplication.class, args);
 	}
